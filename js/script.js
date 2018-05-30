05e015b2cc205cab83b34e371eaf1c997905b2a9
Olexander Vasiliev <vasulevus.work@gmail.com>
2018-05-30 10:45:06 +0300

    Працюючий прототип

--- js/script.js
+++ js/script.js
@@ -16,22 +16,40 @@
         midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
       });
       
-
+      var tabs = $('#tabs');
+      $('.tabs-content > div', tabs).each(function(i){
+          if ( i != 0 ) $(this).hide(0);
+      });
+      tabs.on('click', '.tabs a', function(e){
+          /* Предотвращаем стандартную обработку клика по ссылке */
+          e.preventDefault();
+  
+          /* Узнаем значения ID блока, который нужно отобразить */
+          var tabId = $(this).attr('href');
+  
+          /* Удаляем все классы у якорей и ставим active текущей вкладке */
+          $('.tabs a',tabs).removeClass();
+          $(this).addClass('active');
+  
+          /* Прячем содержимое всех вкладок и отображаем содержимое нажатой */
+          $('.tabs-content > div', tabs).hide(0);
+          $(tabId).show();
+      });
 
     setup_slight_scroll_to_anchors();
    
-    function setup_slight_scroll_to_anchors() {
-        $('a[href^="#"]').not('.popup').click(function() {
-            var link = jQuery(this);
-            if (link.attr('href') == '#') return;
-            var target = link.attr('href');
-            var target_top = jQuery(target).offset().top;
-            jQuery('html, body').animate({
-               scrollTop: target_top
-            }, 1000);
-            return false;
-        });
-    }
+    // function setup_slight_scroll_to_anchors() {
+    //     $('a[href^="#"]').not('.popup').click(function() {
+    //         var link = jQuery(this);
+    //         if (link.attr('href') == '#') return;
+    //         var target = link.attr('href');
+    //         var target_top = jQuery(target).offset().top;
+    //         jQuery('html, body').animate({
+    //            scrollTop: target_top
+    //         }, 1000);
+    //         return false;
+    //     });
+    // }
 
 
     $('.image').slick({
