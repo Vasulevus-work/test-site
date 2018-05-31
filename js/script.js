b55b05b9bd54483a141f7fe659e4220684070472
Olexander Vasiliev <vasulevus.work@gmail.com>
2018-05-31 12:14:18 +0300

    1

--- js/script.js
+++ js/script.js
@@ -2,15 +2,9 @@
 
   $('a[href*="#top"]')
   // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
   .click(function(event) {
     // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
+
       // Figure out element to scroll to
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -28,7 +22,7 @@
 
         });
       }
-    }
+
   });
 
      $('select').niceSelect();
