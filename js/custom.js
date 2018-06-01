11ed09c909d0a7c192cda7110489b8d6c6660e81
Olexander Vasiliev <vasulevus.work@gmail.com>
2018-06-01 14:13:05 +0300

    end of index

--- js/custom.js
+++ js/custom.js
@@ -1,37 +1,41 @@
 // JavaScript Document
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Форма обратной связи................................./
-    
+
+
     var regVr22 = "<div class='messenger_load'>Сообщение обрабатывается...</div><br />";
-    
-    $("#send").click(function(){
-    		$("#loadBar").html(regVr22).show();
-    		var firstName = $("#firstName").val();
-            var textarea = $("#textarea").val();
-            var radio = $("#radio").val();
-			var email = $("#email").val();
-			console.log(radio)
-    		$.ajax({
-    			type: "POST",
-    			url: "mail.php",
-    			data: {"firstName": firstName, "radio": radio, "textarea": textarea, "email": email},
-    			cache: false,
-    			success: function(response){
-    		var messageResp = "<div class='messenger_send'><span>";
-    		var resultStat = ", cпасибо!<span>Ваше сообщение отправлено.</div>";
-    		var oll = (messageResp + firstName + resultStat);
-    				if(response == 1){
-                    $("#loadBar").html(oll).fadeIn(3000);
-    				$("#firstName").val("");
-                    $("#textarea").val("");
-                    $("#radio").val("");
-    				$("#email").val("");
-    				} else {
-    		$("#loadBar").html(response).fadeIn(3000); }
-    										}
-    		});
-    		return false;
+
+    $(".js-quick-order").on("submit", function () {
+        var $this = $(this);
+
+        var $loadbar  = $this.find(".loadBar");
+        $loadbar.html(regVr22).show();
+        var firstName = $this.find(".firstName");
+        var textarea = $this.find(".textarea");
+        var radio = $this.find(".radio");
+        var email = $this.find(".email");
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: {"firstName": firstName.val(), "radio": radio.val(), "textarea": textarea.val(), "email": email.val()},
+            cache: false,
+            success: function (response) {
+                var messageResp = "<div class='messenger_send'><span>";
+                var resultStat = ", cпасибо!<span>Ваше сообщение отправлено.</div>";
+                var oll = (messageResp + firstName.val() + resultStat);
+                if (response == 1) {
+                    $loadbar.html(oll).fadeIn(3000);
+                    firstName.val("");
+                    textarea.val("");
+                    radio.val("");
+                    email.val("");
+                } else {
+                    $loadbar.html(response).fadeIn(3000);
+                }
+            }
+        });
+        return false;
     });
 
 
