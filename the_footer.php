7ce24509db56d838b3436fdb0f69c1f0d7b58c77
vasulevus <vasulevus.work@gmail.com>
2018-06-08 16:04:11 +0300

    grid

---
+++ the_footer.php
@@ -0,0 +1,153 @@
+<footer>
+    <div class="footer-yellback">
+      <div class="container margin-bottom-0 no-padding">
+        <div class="row">
+          <div class="col-sm-12 col-md-5">
+            <h3 class="zagolovok3">Подписаться на рассылку</h3>
+            <p class="big_text big_text-white">Будьте вкурсе
+              <span class="hidden"> самых свежих</span> новостей наших объектов</p>
+          </div>
+          <form class="col-sm-12 col-md-7">
+            <div class="flex-container">
+              <input type="email" class=" form-pad" placeholder="Введите email">
+              <button class="button button-yellow">Подписаться</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="footer-blackback">
+      <div class="container flex-container margin-bottom-0">
+        <div class="column">
+          <ul>
+            <li class="main-li">Строительная компания</li>
+            <li>
+              <a href="">О нас</a>
+            </li>
+            <li>
+              <a href="">Оплата</a>
+            </li>
+            <li>
+              <a href="">Вопрос-ответ</a>
+            </li>
+            <li>
+              <a href="">Акции</a>
+            </li>
+            <li>
+              <a href="">Статьи</a>
+            </li>
+            <li>
+              <a href="">Контакты</a>
+            </li>
+          </ul>
+        </div>
+        <div class="column column-padding">
+          <ul>
+            <li class="main-li">Жилая недвижимость</li>
+            <li>
+              <a href="">ЖК «Два капитала»</a>
+            </li>
+            <li>
+              <a href="">ЖК «Британский дом»</a>
+            </li>
+            <li>
+              <a href="">ЖК «Песочный замок»</a>
+            </li>
+            <li>
+              <a href="">ЖК «Сосновый лес»</a>
+            </li>
+            <li>
+              <a href="">ЖК «Мир минимализма»</a>
+            </li>
+            <li>
+              <a href="">ЖК «Мечта поэта»</a>
+            </li>
+          </ul>
+        </div>
+        <div class="column column-padding2">
+          <ul>
+            <li class="main-li">Коммерческая недвижимость</li>
+            <li>
+              <a href="">БЦ «Два капитала»</a>
+            </li>
+            <li>
+              <a href="">БЦ «Британский дом»</a>
+            </li>
+            <li>
+              <a href="">БЦ «Песочный замок»</a>
+            </li>
+            <li>
+              <a href="">БЦ «Сосновый лес»</a>
+            </li>
+            <li>
+              <a href="">БЦ «Мир минимализма»</a>
+            </li>
+            <li>
+              <a href="">БЦ «Мечта поэта»</a>
+            </li>
+          </ul>
+        </div>
+        <div class="column4">
+          <div class="number1 number1-footer">
+            <a href="tel:8&nbsp;(8482)&nbsp;61-37-37">8&nbsp;(8482)&nbsp;61-37-37</a>
+          </div>
+          <div class="number2 number2-footer">
+            <a href="">dompatriot@gmail.com</a>
+          </div>
+          <p class="text-footer">Россия, г.Тольятти,
+            <br> Приморский бульвар, д.57, подъезд 1</p>
+          <a href="#forma" class="button red popup button-footer">Обратный звонок</a>
+        </div>
+      </div>
+      <div class="container container-footer-padding no-margin">
+        <div class="row justify-content-between">
+          <div class=" col-md-8 col-xl-5">
+            <p class="text-footer text-footer-margin"> 2018 ©
+              <a href=""> Строительная компания «Патриот».</a> Все права защищены.
+              <br> Жилая и коммерческая недвижимость в Тольятти от застройщика</p>
+          </div>
+          <div class="col-md-4 col-lg-3 align-self-center">
+            <div class="padin-top"></div>
+            <div class="row  no-gutters justify-content-between">
+              <div class="col-6 col-sm-4 col-md-8">
+                <a href="">
+                  <i class="fab fa-vk"> </i>
+                </a>
+                <a href="">
+                  <i class="fab fa-facebook-f"> </i>
+                </a>
+                <a href="">
+                  <i class="fab fa-twitter"> </i>
+                </a>
+              </div>
+              <div class="col-2 col-sm-2 col-md-4 mar">
+                <a href="#top">
+                  <i class="fas fa-chevron-up"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Flexnav plugin -->
+
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+  <script src="//yastatic.net/share2/share.js"></script>
+  <script src="js/flexnav.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/jquery.nice-select.min.js"></script>
+  <script src="js/jquery.magnific-popup.js"></script>
+  <script src="js/tab.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+  <script src="js/script1.js"></script>
+  <script src="js/script.js"></script>
+
+</body>
+
+</html>
---
+++ the_header.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,900&amp;subset=cyrillic" rel="stylesheet">
+  <link rel="stylesheet" href="css/bootstrap-grid.css">
+  <!-- Flexnav -->
+  <link rel="stylesheet" href="css/flexnav.css">
+  <!--slick carousel-->
+  <link rel="stylesheet" href="css/slick.css">
+  <!-- Nice select for form -->
+  <link rel="stylesheet" href="css/nice-select.css">
+  <!-- Font awesome -->
+  <link rel="stylesheet" href="css/fontawesome-all.min.css">
+  <!-- Maginific popup -->
+  <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/responsive.css">
+  <title>Site</title>
+</head>
+
+<body>
+  <header>
+    <div class="header-top" id="top">
+      <!--header-top-->
+      <div class="container change">
+        <div class="pack">
+          <div class="row justify-content-around">
+            <div class="col-12 col-md-8 col-lg-5">
+              <div class="row no-gutters">
+                <div class="col-12 col-sm-5">
+                  <div class="logo">
+                    <a href="/">
+                      <img src="img/patriot.png" alt="лого 'Патриот'">
+                    </a>
+                  </div>
+                </div>
+                <div class="col-12 col-sm-7">
+                  <div class="card0">
+                    Жилая и коммерческая недвижимость в Тольятти от застройщика
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-5 col-md-4 col-lg-4 align-self-center">
+              <div class="number1 margin-top-15     margin-left-30">
+                <a href="tel:8&nbsp;(8482)&nbsp;61-37-37">
+                  <div class="h2 margin-top-0 margin-bottom-0"> 8 (8482) 61-37-37</div>
+                </a>
+                <br>
+              </div>
+              <div class="number2">
+                <a href="mailto:dompatriot@gmail.com">dompatriot@gmail.com</a>
+              </div>
+            </div>
+            <div class="col-12 col-sm-7 col-md-4 col-lg-3">
+              <a href="#forma" class="button red popup" data-effect="mfp-zoom-in">Обратный звонок</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- header -container  -->
+    </div>
+    <!--/header-top-->
+    <div class="header-bottom grayback">
+      <!--header-bottom-->
+      <div class="container cont1">
+        <div class="row no-gutters justify-content-between">
+          <div class="button blue menu-button">
+            <div>МЕНЮ</div>
+          </div>
+          <div class="col-12 col-sm-7 col-md-4 col-lg-3">
+            <a href="#forma" class="button red show popup" data-effect="mfp-zoom-in">Обратный звонок</a>
+          </div>
+          <ul class="flexnav col-12" data-breakpoint="991">
+            <li>
+              <a href="">Жилая недвижимость</a>
+            </li>
+            <li>
+              <a href="">Коммерческая недвижимость</a>
+            </li>
+            <li>
+              <a href="">О нас</a>
+            </li>
+            <li>
+              <a href="">Оплата</a>
+            </li>
+            <li>
+              <a href="">Вопрос-ответ</a>
+            </li>
+            <li>
+              <a href="">Акции</a>
+            </li>
+            <li>
+              <a href="">Статьи</a>
+            </li>
+            <li>
+              <a href="#contact">Контакты</a>
+            </li>
+          </ul>
+        </div>
+        <!--row-->
+      </div>
+      <!--container-->
+    </div>
+    <!--header-bottom-->
+  </header>
